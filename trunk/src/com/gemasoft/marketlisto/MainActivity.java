1b82d781bced7a67e84fb43b0ca4f97e2608f657
DsxConcept@gmail.com <DsxConcept@gmail.com@56cd8885-561e-6db6-6d43-611aea018f4c>
2012-10-31 14:36:56 +0000

    Items list added

--- trunk/src/com/gemasoft/marketlisto/MainActivity.java
+++ trunk/src/com/gemasoft/marketlisto/MainActivity.java
@@ -1,19 +1,34 @@
 package com.gemasoft.marketlisto;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+//import android.content.Intent;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	 	//LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+	 	ArrayList<String> listTODO = PrepareList();
+	    //DEFINING STRING ADAPTER WHICH WILL HANDLE DATA OF LISTVIEW
+	    ArrayAdapter<String> adapter;
 	public final static String EXTRA_MESSAGE = "com.gemasoft.MarketListo.MESSAGE";
 
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     		// Get a handle to the list view
+     		ListView lv = (ListView) findViewById(R.id.mainlistView);
+
+     		// Bind the data with the list     		
+     		adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listTODO);
+     		lv.setAdapter(adapter);			
     }
 
     @Override
@@ -23,10 +38,31 @@ public class MainActivity extends Activity {
     }
     
     public void sendMessage(View view){
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	//Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	//EditText editText = (EditText) findViewById(R.id.edit_message);
+    	//String message = editText.getText().toString();
+    	//intent.putExtra(EXTRA_MESSAGE,  message);
+    	//startActivity(intent);
+    }
+    
+ // The main ArrayList .
+ 	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private ArrayList<String> PrepareList() {
+ 		ArrayList todoItems = new ArrayList();
+ 		todoItems.add("Market list");
+ 		todoItems.add("Homework");
+ 		todoItems.add("Pending work tasks");
+ 		todoItems.add("Taxes");
+ 		return todoItems;
+
+ 	}
+ 	
+ 	//METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItem(View v) {
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE,  message);
-    	startActivity(intent);
+    	listTODO.add(message);
+        adapter.notifyDataSetChanged();
+        editText.setText("");
     }
 }
