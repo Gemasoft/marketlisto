cff5e4a18d5b188251d1b2924082f638ef953a90
DsxConcept@gmail.com <DsxConcept@gmail.com@56cd8885-561e-6db6-6d43-611aea018f4c>
2012-11-01 00:41:56 +0000

    -Custom List Item Added listitem_custom_row.xml -Custom adapter Added ListItemAdapter.class -custom List Item class Added ListItem.class

--- trunk/gen/com/gemasoft/marketlisto/R.java
+++ trunk/gen/com/gemasoft/marketlisto/R.java
@@ -16,32 +16,57 @@ public final class R {
         public static final int ic_splash=0x7f020002;
     }
     public static final class id {
-        public static final int edit_message=0x7f070000;
-        public static final int imageView1=0x7f070002;
-        public static final int mainlistView=0x7f070001;
-        public static final int menu_settings=0x7f070003;
+        public static final int TableLayout01=0x7f080006;
+        public static final int btnClear=0x7f080005;
+        public static final int btnSave=0x7f080004;
+        public static final int btnSend=0x7f080008;
+        public static final int buttonlayout=0x7f080000;
+        public static final int buttonlayout2=0x7f080001;
+        public static final int buttonlayout3=0x7f080003;
+        public static final int cbxChecked=0x7f08000b;
+        public static final int edit_message=0x7f080007;
+        public static final int imageView1=0x7f08000a;
+        public static final int mainlistView=0x7f080009;
+        public static final int menu_settings=0x7f08000d;
+        public static final int txtTest=0x7f080002;
+        public static final int txtTitle=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_splash_screen=0x7f030002;
+        public static final int activity_list_selector=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_splash_screen=0x7f030003;
+        public static final int listitem_custom_row=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_display_message=0x7f060000;
-        public static final int activity_main=0x7f060001;
-        public static final int activity_splash_screen=0x7f060002;
+        public static final int activity_display_message=0x7f070000;
+        public static final int activity_list_selector=0x7f070001;
+        public static final int activity_main=0x7f070002;
+        public static final int activity_simple=0x7f070003;
+        public static final int activity_splash_screen=0x7f070004;
+        public static final int listitem_custom_row=0x7f070005;
     }
     public static final class string {
-        public static final int app_description=0x7f040006;
-        public static final int app_name=0x7f040000;
-        public static final int button_send=0x7f040004;
-        public static final int edit_message=0x7f040003;
-        public static final int menu_settings=0x7f040001;
-        public static final int title_activity_display_message=0x7f040005;
-        public static final int title_activity_main=0x7f040002;
-        public static final int title_activity_splash_screen=0x7f040007;
+        public static final int app_description=0x7f050006;
+        public static final int app_name=0x7f050000;
+        public static final int button_clear=0x7f05000b;
+        public static final int button_save=0x7f05000c;
+        public static final int button_send=0x7f050004;
+        public static final int edit_message=0x7f050003;
+        public static final int hello_world=0x7f050008;
+        public static final int list_header=0x7f05000a;
+        public static final int menu_settings=0x7f050001;
+        public static final int title_activity_custom_row=0x7f05000e;
+        public static final int title_activity_display_message=0x7f050005;
+        public static final int title_activity_list_selector=0x7f05000d;
+        public static final int title_activity_main=0x7f050002;
+        public static final int title_activity_simple=0x7f050009;
+        public static final int title_activity_splash_screen=0x7f050007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int todolist=0x7f040000;
     }
 }
