4322785da6c73f70787e8b2d71dd7e35fcc7e42a
DsxConcept@gmail.com <DsxConcept@gmail.com@56cd8885-561e-6db6-6d43-611aea018f4c>
2012-11-06 08:25:53 +0000

    Now working with SQLite
    Added add, delete and check functionalities.

--- gen/com/gemasoft/marketlisto/R.java
+++ gen/com/gemasoft/marketlisto/R.java
@@ -11,57 +11,63 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_splash=0x7f020002;
+        public static final int btn_check_off=0x7f020000;
+        public static final int btn_check_on=0x7f020001;
+        public static final int checkbox_selector=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_splash=0x7f020005;
+        public static final int icon=0x7f020006;
     }
     public static final class id {
-        public static final int TableLayout01=0x7f080006;
+        public static final int btnAddNewItem=0x7f080003;
+        public static final int btnCancelAdd=0x7f080002;
         public static final int btnClear=0x7f080005;
-        public static final int btnSave=0x7f080004;
-        public static final int btnSend=0x7f080008;
-        public static final int buttonlayout=0x7f080000;
-        public static final int buttonlayout2=0x7f080001;
-        public static final int buttonlayout3=0x7f080003;
-        public static final int cbxChecked=0x7f08000b;
-        public static final int edit_message=0x7f080007;
-        public static final int imageView1=0x7f08000a;
-        public static final int mainlistView=0x7f080009;
-        public static final int menu_settings=0x7f08000d;
-        public static final int txtTest=0x7f080002;
-        public static final int txtTitle=0x7f08000c;
+        public static final int btnSend=0x7f080004;
+        public static final int cbxChecked=0x7f080008;
+        public static final int imageView1=0x7f080007;
+        public static final int mainlistView=0x7f080006;
+        public static final int menu_settings=0x7f080009;
+        public static final int textView1=0x7f080000;
+        public static final int txtNewItemTitle=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_list_selector=0x7f030001;
+        public static final int activity_add_item=0x7f030000;
+        public static final int activity_display_message=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_splash_screen=0x7f030003;
         public static final int listitem_custom_row=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_display_message=0x7f070000;
-        public static final int activity_list_selector=0x7f070001;
-        public static final int activity_main=0x7f070002;
-        public static final int activity_simple=0x7f070003;
-        public static final int activity_splash_screen=0x7f070004;
-        public static final int listitem_custom_row=0x7f070005;
+        public static final int activity_add_item=0x7f070000;
+        public static final int activity_display_message=0x7f070001;
+        public static final int activity_list_selector=0x7f070002;
+        public static final int activity_main=0x7f070003;
+        public static final int activity_read_from_database=0x7f070004;
+        public static final int activity_simple=0x7f070005;
+        public static final int activity_splash_screen=0x7f070006;
+        public static final int listitem_custom_row=0x7f070007;
     }
     public static final class string {
-        public static final int app_description=0x7f050006;
+        public static final int app_description=0x7f050005;
         public static final int app_name=0x7f050000;
-        public static final int button_clear=0x7f05000b;
-        public static final int button_save=0x7f05000c;
-        public static final int button_send=0x7f050004;
+        public static final int button_add_new=0x7f050011;
+        public static final int button_add_new_item=0x7f05000f;
+        public static final int button_cancel_new_item=0x7f050010;
+        public static final int button_clear=0x7f050009;
+        public static final int button_save=0x7f05000a;
         public static final int edit_message=0x7f050003;
-        public static final int hello_world=0x7f050008;
-        public static final int list_header=0x7f05000a;
+        public static final int list_header=0x7f050008;
         public static final int menu_settings=0x7f050001;
-        public static final int title_activity_custom_row=0x7f05000e;
-        public static final int title_activity_display_message=0x7f050005;
-        public static final int title_activity_list_selector=0x7f05000d;
+        public static final int textbox_new_item_title=0x7f050012;
+        public static final int title_activity_add_item=0x7f05000e;
+        public static final int title_activity_custom_row=0x7f05000c;
+        public static final int title_activity_display_message=0x7f050004;
+        public static final int title_activity_list_selector=0x7f05000b;
         public static final int title_activity_main=0x7f050002;
-        public static final int title_activity_simple=0x7f050009;
-        public static final int title_activity_splash_screen=0x7f050007;
+        public static final int title_activity_read_from_database=0x7f05000d;
+        public static final int title_activity_simple=0x7f050007;
+        public static final int title_activity_splash_screen=0x7f050006;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
